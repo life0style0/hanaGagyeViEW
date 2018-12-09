ce2f68659b7d9f347ab43cf5797af744fb3f00d3
Chicpork <dkgkrmfo@gmail.com>
2018-12-09 14:30:33 +0900

    Apply .gitignore

--- src/main/java/kr/or/kosta/salmon/service/ImageService.java
+++
@@ -1,9 +0,0 @@
-package kr.or.kosta.salmon.service;
-
-/**
- * ImageService
- */
-public interface ImageService {
-
-    public boolean registProfileImage(String path, String userId) throws Exception;
-}
